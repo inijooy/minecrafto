d085e21d1949c9125aea7dd6c260fc085a05bfd6
Nocturnal <serotoninoy@gmail.com>
2025-02-11 23:12:33 +0700

    cancel chatSend event

--- behavior_pack/scripts/main.js
+++ behavior_pack/scripts/main.js
@@ -1,7 +1,7 @@
 import { world } from "@minecraft/server";
 
 world.beforeEvents.chatSend.subscribe((ctx) => {
-    ctx.cancel;
+    ctx.cancel = true;
     world.sendMessage(
         `\u00A7e${ctx.sender.name} \u00A78| \u00A77${ctx.message}`,
     );
