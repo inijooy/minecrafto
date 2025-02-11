df5dd0d6427178196b80c9a428a61f7b0cea12fe
Nocturnal <serotoninoy@gmail.com>
2025-02-11 22:36:13 +0700

    put script entry

--- behavior_pack/scripts/main.js
+++ behavior_pack/scripts/main.js
@@ -2,5 +2,7 @@ import { world } from "@minecraft/server";
 
 world.beforeEvents.chatSend.subscribe((ctx) => {
     ctx.cancel;
-    world.sendMessage(`\u00A7e${ctx.sender.name} \u00A78| \u00A77${ctx.message}`);
+    world.sendMessage(
+        `\u00A7e${ctx.sender.name} \u00A78| \u00A77${ctx.message}`,
+    );
 });
